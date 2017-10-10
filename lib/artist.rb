f5585b39450acc9a17880f3d7207a71f805f9b73
Matt Hough <matt@hough.tv>
2017-10-10 11:22:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
     if self.all.empty?
       self.create(name)
     else
-      self.all.detect {}|artist| artist.name == name}
+      self.all.detect {|artist| artist.name == name}
       end
     end
   end
