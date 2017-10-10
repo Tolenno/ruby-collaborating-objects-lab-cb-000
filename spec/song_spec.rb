06393a831e66bf5cc7af8408c822ab359c8944e4
Matt Hough <matt@hough.tv>
2017-10-10 10:44:26 +0000

    automatically backed up by learn

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -25,7 +25,7 @@ describe 'Song' do
 
   describe '.new_by_filename' do
     it 'creates a new instance of a song from the file that\'s passed in' do
-      binding.pr
+      binding.pry
       new_instance = Song.new_by_filename(file_name)
       expect(new_instance.name).to eq('Black or White')
       expect(new_instance.artist.name).to eq('Michael Jackson')
