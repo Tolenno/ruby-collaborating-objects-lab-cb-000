a20f7b3fe8d95c5fcd2f671298ce8e30e84ac3d0
Matt Hough <matt@hough.tv>
2017-10-10 10:28:10 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.empty?
+      self.create(name)
+    else
+      self.all.detect do |artist|
+        artist.name == name ? true : self.create(name)
+      end
+    end
+  end
+
+  def self.create(name)
+    self.new(name).tap(&:save)
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+end
